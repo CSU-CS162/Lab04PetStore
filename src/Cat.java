5850ec5c7f5607896ebc92eb3ac3400560b192b4
Emilie <98420041+themilkprince@users.noreply.github.com>
2022-06-22 16:42:13 -0600

    Create Cat.java

---
+++ src/Cat.java
@@ -0,0 +1,6 @@
+public class Cat extends Pets {
+    public Cat(String name, String breed){
+      //TODO Student Code
+    }
+    //TODO Student Code -- Write an override for makeNoise() and for getName()
+}
